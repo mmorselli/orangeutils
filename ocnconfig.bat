a8f07062015028a212982d9dc26213321670a56b
mmorselli <massimo@morselli.com>
2024-01-03 22:51:48 +0100

    exe is no longer mandatory

--- ocnconfig.bat
+++ ocnconfig.bat
@@ -1,6 +1,10 @@
 @echo off
 cd /d %~dp0
 
-ocnconfig.exe
+if exist ocnconfig.exe (
+    ocnconfig.exe mainnet --tpm 1 --fee 2000
+) else (
+    python ocnconfig.py mainnet --tpm 1 --fee 2000
+)
 
 PAUSE
