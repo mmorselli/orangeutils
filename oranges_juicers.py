97913840858ae7f62a298438053a8f3ae9cbd7d9
mmorselli <massimo@morselli.com>
2024-01-10 17:12:53 +0100

    Add files via upload

--- oranges_juicers.py
+++ oranges_juicers.py
@@ -38,40 +38,48 @@ def orange_count(address):
 
     return asset_balance
 
+# algod stores only the last 1000 blocks
+def juicers_in_round(round_num):
+    count = 0
+    juicers = ""
+    winner = None
+    oranges = 0
+    unique_snd = set() # set of unique senders
+    block_info = ALGOD_CLIENT.block_info(round_num)
+    for tx in block_info['block']['txns']:
+        # print(json.dumps(tx, indent=4))
+        if 'apid' in tx['txn'] and tx['txn']['apid'] == app_id:
+            if tx['txn']['snd'] != 'ORANGE46RBK7YJRWGGLMMCYJ2NZOJG3WAE2MCRSCK5ZDOJ5RKAZAE5P554':
+                count += 1
+                snd = tx['txn']['snd']
+                unique_snd.add(snd)
+            else:
+                winner = tx['txn']['apat'][0]
+                oranges = orange_count(winner)
+    juicers += f"Number of transactions: {count}\n"
+    juicers += f"Juicers: {len(unique_snd)}\n"
+    for i, snd in enumerate(unique_snd, start=1):
+        juicers += f"{i}) {snd}\n"
+
+    return juicers, winner, oranges
+
 
 def parse_juicers(maxround, print_juicers=True, print_winner=True):
     roundparsed = 0
     previous_round = None
     while roundparsed < maxround:
-        juicers = ""
         last_round = ALGOD_CLIENT.status()['last-round']
         if last_round == previous_round:
             continue
         previous_round = last_round
         roundparsed += 1
-        count = 0
-        unique_snd = set() # set of unique senders
         blocknum = f"BLOCK: {last_round}"
-        block_info = ALGOD_CLIENT.block_info(last_round)
-        for tx in block_info['block']['txns']:
-            # print(json.dumps(tx, indent=4))
-            if 'apid' in tx['txn'] and tx['txn']['apid'] == app_id:
-                if tx['txn']['snd'] != 'ORANGE46RBK7YJRWGGLMMCYJ2NZOJG3WAE2MCRSCK5ZDOJ5RKAZAE5P554':
-                    count += 1
-                    snd = tx['txn']['snd']
-                    unique_snd.add(snd)
-                else:
-                    winner = tx['txn']['apat'][0]
-                    oranges = orange_count(winner)
-                    formatted_oranges = "{:,.2f}".format(oranges / 100000000)
-        juicers += f"Number of transactions: {count}\n"
-        juicers += f"Juicers: {len(unique_snd)}\n"
-        for i, snd in enumerate(unique_snd, start=1):
-            juicers += f"{i}) {snd}\n"
+        juicers, winner, oranges = juicers_in_round(last_round)
         print(blocknum)
         if print_juicers:
             print(juicers)
-        if print_winner:
+        if winner and print_winner:
+            formatted_oranges = "{:,.2f}".format(oranges / 100000000)
             print(f"Winner: {winner} (Balance: {formatted_oranges} ORA)")
         time.sleep(1)
 
